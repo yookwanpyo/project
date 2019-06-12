40345572b9144a79ead7bb865c0f4e8a01ecf7ae
ykp <yookwanpyo@naver.com>
2019-06-12 11:05:13 +0900

    Updates

---
+++ precache-manifest.5d10ac302b76e3a6243e10944b3c8cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25efc5f6af462d2f7bc4d85e1814b993",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "c2a076a47a6cc0124815",
+    "url": "/project/static/css/main.a57c339c.chunk.css"
+  },
+  {
+    "revision": "b0c5012ca7746f497091",
+    "url": "/project/static/js/2.ab1b0ea4.chunk.js"
+  },
+  {
+    "revision": "c2a076a47a6cc0124815",
+    "url": "/project/static/js/main.c57001a0.chunk.js"
+  },
+  {
+    "revision": "fa0407e1e655c72c7c52",
+    "url": "/project/static/js/runtime~main.ceb979a2.js"
+  }
+]);
